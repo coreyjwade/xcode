9345c1d8d57f4a759e638a4bf47324c74dfb69dc
Corey Wade <coreyjwade@coreys-macbook-pro.local>
2018-04-17 23:02:03 -0700

    updated clock with timer

--- myClock/ViewController.swift
+++ myClock/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //This is our label to display the current time
+    @IBOutlet weak var currentTime: UILabel! {
+        //We set the label to monospace so that it doesn't jump
+        didSet {
+            currentTime.font = UIFont.monospacedDigitSystemFont(ofSize: currentTime!.font!.pointSize, weight: UIFont.Weight.regular)
+        }
+    }
+    
+    //Dates are conveniently expressed with a DateFormatter()
+    let myFormatter = DateFormatter()
 
+    //We call this function every tenth of a second
+    @objc func updateTimer() {
+        // Date() gives the current Date and Time
+        let now = Date()
+        //Set our label to the DateFormatter()
+        currentTime.text = myFormatter.string(from: now)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Select the style of the formatter upon loading
+        myFormatter.timeStyle = .medium
+        //Initiate the timer
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
